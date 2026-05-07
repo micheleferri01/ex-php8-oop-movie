30fdc614d19b4493b483d363167e9f72207afa55
micheleferri01 <micheleferri095@gmail.com>
2026-05-07 13:15:28 +0200

    add: method function to class Movie

--- Models/movie.php
+++ Models/movie.php
@@ -7,9 +7,13 @@
         public $genre;
 
         public function __construct($title,$director,$year,$genre) {
-            this-> title = $title;
-            this-> director = $director;
-            this-> year = $year;
-            this-> genre = $genre;
+            $this-> title = $title;
+            $this-> director = $director;
+            $this-> year = $year;
+            $this-> genre = $genre;
+        }
+
+        public function getMovieInfos () {
+            return "{$this->title} diretto da {$this->director} ({$this->year}) - Genere: {$this->genre}";
         }
     }
