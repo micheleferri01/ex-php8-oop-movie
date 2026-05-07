30ff22241919e3fbf9d58c40138f8edf384d02cf
micheleferri01 <micheleferri095@gmail.com>
2026-05-07 13:35:00 +0200

    imported classes

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
+
+require_once('./Models/genre.php');
+
+$action = new Genre("Azione", "Film ricchi di combattimenti e adrenalina");
+$comedy = new Genre("Commedia", "Film divertenti e leggeri");
+
 require_once('./Models/movie.php');
+
+$movie1 = new Movie("Inception", "Christopher Nolan", 2010, $action);
+$movie2 = new Movie("Una notte da leoni", "Todd Phillips", 2009, $comedy);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +19,9 @@ require_once('./Models/movie.php');
     <title>ex-php8-oop-movie</title>
 </head>
 <body>
-    
+    <?php
+        echo "<p>" . $movie1->getMovieInfos() . "</p>";
+        echo "<p>" . $movie2->getMovieInfos() . "</p>";
+    ?>
 </body>
 </html>
