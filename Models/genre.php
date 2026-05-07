f0705ebb83a988e84616f6a400f6af09dabcd9a7
micheleferri01 <micheleferri095@gmail.com>
2026-05-07 13:21:07 +0200

    add: construct function to class Genre

--- Models/genre.php
+++ Models/genre.php
@@ -3,4 +3,9 @@
 class Genre {
     public $name;
     public $description;
+
+    public function __construct ($name, $description) {
+        $this-> name = $name;
+        $this-> description = $description;
+    }
 }
