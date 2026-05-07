019e4245363e72822c359c3ddef00364811cbb35
micheleferri01 <micheleferri095@gmail.com>
2026-05-07 12:25:02 +0200

    add: construct function to class movie

--- Models/movie.php
+++ Models/movie.php
@@ -5,4 +5,11 @@
         public $director;
         public $year;
         public $genre;
+
+        public function __construct($title,$director,$year,$genre) {
+            this-> title = $title;
+            this-> director = $director;
+            this-> year = $year;
+            this-> genre = $genre;
+        }
     }
